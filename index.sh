dc6a60321299269c5433fae40125616949681611
kk4466345 <kk4466345@gmail.com>
2018-08-18 15:28:38 +0530

    add photos and index.sh

---
+++ index.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# Passing arguments to a function
+print_something () {
+num1=$1
+num2=1
+num=`expr $num1 + $num2`  
+sleep 3.0
+kkm=$(ls $num.*)
+/usr/bin/gsettings set org.gnome.desktop.background picture-uri file:///$(pwd)/$kkm
+echo $kkm
+        if [ $num -eq 7 ]
+        then
+                num=0
+                print_something $num
+	else	
+		print_something $num
+
+        fi
+
+}
+
+
+
+print_something 0
